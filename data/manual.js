3fa1b3ce9d1296c631fa38a8ff3f88d57a168747
memory-agape <memory.style.serv@gmail.com>
2019-08-17 19:16:38 +0900

    Add localizations

--- data/manual.js
+++ data/manual.js
@@ -1,4 +1,12 @@
 const manual = {
   0: 'https://www.php.net/manual/en/',
   1: 'https://www.php.net/manual/ja/',
+  2: 'https://www.php.net/manual/pt_BR/',
+  3: 'https://www.php.net/manual/zh/',
+  4: 'https://www.php.net/manual/fr/',
+  5: 'https://www.php.net/manual/de/',
+  6: 'https://www.php.net/manual/ro/',
+  7: 'https://www.php.net/manual/ru/',
+  8: 'https://www.php.net/manual/es/',
+  9: 'https://www.php.net/manual/tr/',
 };
--- page.js
+++ page.js
@@ -1,27 +1,32 @@
-function update(propertyName) {
+const updateText = (propertyName, value) => {
+  const propertyTarget = propertyName === 'languageId'
+    ? ['language', 'data-language-id']
+    : ['position', 'data-position-id'];
+
+  console.log(`.choose-${propertyTarget[0]} [${propertyTarget[1]}="${value}"]`);
+  document.querySelector(`.choose-${propertyTarget[0]} .active`).classList.remove('active');
+  document.querySelector(`.choose-${propertyTarget[0]} [${propertyTarget[1]}="${value}"]`).classList.add('active');
+};
+
+const update = (propertyName) => {
   chrome.storage.sync.get(
     ['languageId', 'positionId'],
     (items) => {
       const value = items[propertyName] || 0;
-
-      const propertyTarget = propertyName === 'languageId'
-        ? ['language', 'data-language-id']
-        : ['position', 'data-position-id'];
-
-      document.querySelector(`.choose-${propertyTarget[0]} .active`).classList.remove('active');
-      document.querySelector(`.choose-${propertyTarget[0]} [${propertyTarget[1]}="${value}"]`).classList.add('active');
+      updateText(propertyName, value);
     }
   );
-}
-function change(propertyName, value) {
+};
+
+const change = (propertyName, value) => {
   chrome.storage.sync.set({
     [propertyName]: value,
   });
-  update(propertyName);
+  updateText(propertyName, value);
 
   document.querySelector('.description').classList.add('disabled');
   document.querySelector('.flash-message').classList.remove('disabled');
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   update('languageId');
@@ -30,9 +35,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // Update languages
   document.querySelectorAll('[data-language-id]').forEach((element) => {
     element.addEventListener('click', (e) => {
-      const languageId = e.path[1].getAttribute('data-language-id') * 1;
+      const languageId = element.getAttribute('data-language-id') * 1;
       change(
-        'languageId', languageId
+        'languageId',
+        languageId
       );
     })
   });
@@ -40,10 +46,17 @@ document.addEventListener('DOMContentLoaded', () => {
   // Update positions
   document.querySelectorAll('[data-position-id]').forEach((element) => {
     element.addEventListener('click', (e) => {
-      const positionId = e.path[1].getAttribute('data-position-id') * 1;
+      const positionId = element.getAttribute('data-position-id') * 1;
       change(
-        'positionId', positionId
+        'positionId',
+        positionId
       );
     })
+  });
+
+  // Translation
+  document.querySelectorAll('[data-translate]').forEach((element) => {
+    const text = element.getAttribute('data-translate');
+    element.innerHTML = chrome.i18n.getMessage(text);
   });
 });
